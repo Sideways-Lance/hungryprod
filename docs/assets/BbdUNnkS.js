df9536d6ee2df95e04b7e5905968369a6d584107
mykee3991 <myllsemerrill.casiple@gmail.com>
2025-03-27 22:14:53 +0800

    version 4

---
+++ docs/assets/BbdUNnkS.js
@@ -0,0 +1,1 @@
+import{u as t}from"./CgQNONnI.js";import{o as e,c as o,J as l}from"./CSMtM7qk.js";const n={class:"layout-wrapper layout-blank","data-allow-mismatch":""},p={__name:"blank",setup(r){const{injectSkinClasses:a}=t();return a(),(s,c)=>(e(),o("div",n,[l(s.$slots,"default")]))}};export{p as default};
